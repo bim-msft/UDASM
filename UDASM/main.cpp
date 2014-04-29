276e7bea739a33d004500a941b05779bd73b1238
Uranux <michaelalan@yeah.net>
2014-04-29 21:09:14 +0800

    Initial Commit

---
+++ UDASM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  UDASM
+//
+//  Created by Uranux on 14-4-29.
+//  Copyright (c) 2014年 Uranux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
